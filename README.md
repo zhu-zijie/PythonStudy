380cb5151b4704c0fd6fb844fcaf50b87ba00070
zhuzijie <zhuzijier@outlook.com>
2023-01-06 14:03:09 +0800

    修改一些代码

--- README.md
+++
@@ -1,3 +0,0 @@
-# python_study
-主要记录学习python
-2022.03.09-
--- Test/爬虫/第三节/test06.py
+++
@@ -1,9 +0,0 @@
-# 让selenium启动谷歌浏览器
-from selenium.webdriver import Chrome
-
-# 1.创建浏览器对象
-web = Chrome()
-# 2.打开一个网址
-web.get("http://www.baidu.com")
-
-print(web.title)
