3da8f2aeba0850ddeb413fffa936ff55dcadcaa4
zijie <68392315+zhu-zijie@users.noreply.github.com>
2023-12-20 19:17:16 +0800

    Update test02.py

--- 基础知识/第一节/test02.py
+++ 基础知识/第一节/test02.py
@@ -1,6 +1,6 @@
 import requests
 
-query = input("请输入你喜爱的明显：")
+query = input("请输入你喜爱的明星：")
 url = f"https://www.sogou.com/web?query={query}"
 header = {
 "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36"
