67864d6b5de5c0a452538920fa5be3f32c295fb6
zhu-zijie <2672884775@qq.com>
2024-06-21 19:39:31 +0800

    fix a bug

--- test02/demo/demo1.py
+++ test02/demo/demo1.py
@@ -3,6 +3,8 @@
 # @Author: zijier
 # @Desc: 聊天室客户端
 # @Date: 20:01 2024/04/13
+import threading
+from socket import *
 
 import wx
 
@@ -18,7 +20,7 @@ class Client(wx.Frame):
         g1 = wx.FlexGridSizer(wx.HORIZONTAL)  # 可伸缩的网格布局，水平方向
         # 创建两个按钮
         conn_button = wx.Button(p1, size=(200, 40), label='连接')
-        disconn_button = wx.Button(p1, size=(200, 40), label='断开')
+        disconn_button = wx.Button(p1, size=(200, 40), label='离开')
         g1.Add(conn_button, 1, wx.TOP | wx.LEFT)  # 连接按钮布局在左边
         g1.Add(disconn_button, 1, wx.TOP | wx.RIGHT)  # 断开按钮布局在右边
         box.Add(g1, 1, wx.ALIGN_CENTRE)  # 联合居中
@@ -28,8 +30,8 @@ class Client(wx.Frame):
         box.Add(self.text, 1, wx.ALIGN_CENTRE)
 
         # 创建聊天的输入文本框，可以写
-        self.text = wx.TextCtrl(p1, size=(400, 100), style=wx.TE_MULTILINE)
-        box.Add(self.text, 1, wx.ALIGN_CENTRE)
+        self.input_text = wx.TextCtrl(p1, size=(400, 100), style=wx.TE_MULTILINE)
+        box.Add(self.input_text, 1, wx.ALIGN_CENTRE)
 
         # 最后创建两个按钮，分别是发送和重置
         g2 = wx.FlexGridSizer(wx.HORIZONTAL)
@@ -39,14 +41,67 @@ class Client(wx.Frame):
         g2.Add(send_button, 1, wx.TOP | wx.RIGHT)
         box.Add(g2, 1, wx.ALIGN_CENTRE)
 
-        # 给所有按钮绑定点击事件
-        wx.Bind(wx.EVT_BUTTON, self.connect_to_server, conn_button)
+        p1.SetSizer(box)
 
+        # 给所有按钮绑定点击事件
+        self.Bind(wx.EVT_BUTTON, self.connect_to_server, conn_button)
+        self.Bind(wx.EVT_BUTTON, self.send_to, send_button)
+        self.Bind(wx.EVT_BUTTON, self.go_out, disconn_button)
+        self.Bind(wx.EVT_BUTTON, self.reset, clear_button)
+
+        # 客户端属性
+        self.name = c_name
+        self.isConnected = False
+        self.client_socket = None
+
+    # 连接服务器
     def connect_to_server(self, event):
-        pass
+        print(f"客户端{self.name}，开始连接服务器！")
+        if not self.isConnected:
+            server_host_port = ('localhost', 8888)
+            self.client_socket = socket(AF_INET, SOCK_STREAM)
+            self.client_socket.connect(server_host_port)
+            # 客户端只要连接成功，马上把自己的名字发给服务器
+            self.client_socket.send(self.name.encode('utf-8'))
+            self.isConnected = True
+            t = threading.Thread(target=self.receive_data)
+            t.setDaemon(True)  # 客户端UI界面关闭，当前守护线程也自动关闭
+            t.start()
+
+    # 接受服务器发送过来的聊天数据
+    def receive_data(self):
+        while self.isConnected:
+            try:
+                data = self.client_socket.recv(1024).decode('utf-8')
+                # 从服务器接收到的数据显示
+                self.text.AppendText(f"{data}\n")
+            except ConnectionResetError:
+                print(f"客户端 {self.name} 意外断开连接。")
+                self.isConnected = False
+                break
+
+    # 客户端发送消息到聊天室
+    def send_to(self):
+        if self.isConnected:
+            info = self.input_text.GetValue()
+            if info != '':
+                self.client_socket.send(info.encode('utf-8'))
+                # 输入框的数据如果已经发送了，输入框重新为空
+                self.input_text.SetValue('')
+
+    # 客户端离开聊天
+    def go_out(self, event):
+        self.client_socket.send('A^disconnect^B'.encode('utf-8'))
+        # 客户端主线程也关闭
+        self.isConnected = False
+
+    # 客户端输入框的信息重置
+    def reset(self, event):
+        self.input_text.Clear()
 
 
 if __name__ == '__main__':
     app = wx.App()
-    Client('张三').Show()
+    name = input("请输入客户端的名字：")
+    Client(name).Show()
     app.MainLoop()  # 循环刷新显示
--- test02/demo/demo2.py
+++ test02/demo/demo2.py
@@ -4,6 +4,7 @@
 # @Desc: 聊天室服务端
 # @Date: 20:34 2024/04/13
 import threading
+import time
 
 import wx
 from socket import *
@@ -12,17 +13,17 @@ from socket import *
 class Server(wx.Frame):
     def __init__(self):
         # 调用父类的初始化函数
-        wx.Frame.__init__(self, None, id=101, title="子杰聊天客户端", pos=wx.DefaultPosition, size=(400, 470))
+        wx.Frame.__init__(self, None, id=101, title="子杰的聊天服务端", pos=wx.DefaultPosition, size=(400, 470))
         p1 = wx.Panel(self)  # 在窗口中初始化一个面板
         # 在面板中放一些按钮，文本框，文本输入框，将这些对象统一放进一个盒子里
         box = wx.BoxSizer(wx.VERTICAL)  # 在盒子里面垂直方向自动排版
         g1 = wx.FlexGridSizer(wx.HORIZONTAL)  # 可伸缩的网格布局，水平方向
-        # 创建两个按钮
-        start_server__button = wx.Button(p1, size=(130, 40), label='启动')
-        record_save__button = wx.Button(p1, size=(130, 40), label='聊天记录保存')
+        # 创建三个按钮
+        start_server_button = wx.Button(p1, size=(130, 40), label='启动')
+        record_save_button = wx.Button(p1, size=(130, 40), label='聊天记录保存')
         stop_server_button = wx.Button(p1, size=(130, 40), label='停止')
-        g1.Add(start_server__button, 1, wx.TOP)
-        g1.Add(record_save__button, 1, wx.TOP)
+        g1.Add(start_server_button, 1, wx.TOP)
+        g1.Add(record_save_button, 1, wx.TOP)
         g1.Add(stop_server_button, 1, wx.TOP)
         box.Add(g1, 1, wx.ALIGN_CENTRE)  # 联合居中
         # 创建只读的文本框，显示聊天记录
@@ -39,7 +40,8 @@ class Server(wx.Frame):
         self.session_thread_map = {}  # 存放所有的服务器会话线程，客户端名字为key， 会话线程为value
 
         # 给所有的按钮绑定相应的动作
-        self.Bind(wx.EVT_BUTTON, self.start_server, start_server__button)  # 给启动按钮绑定一个事件，事件触发会自动调用一个函数
+        self.Bind(wx.EVT_BUTTON, self.start_server, start_server_button)  # 给启动按钮绑定一个事件，事件触发会自动调用一个函数
+        self.Bind(wx.EVT_BUTTON, self.save_record, record_save_button)
 
     def start_server(self, event):
         print("服务器启动！")
@@ -47,7 +49,7 @@ class Server(wx.Frame):
             # 启动服务器的主线程
             self.isOn = True
             main_thread = threading.Thread(target=self.do_work)
-            main_thread.setDaemon(self)  # 设置为守护线程
+            main_thread.setDaemon(True)  # 设置为守护线程
             main_thread.start()
 
     # 服务器运行之后的函数
@@ -58,10 +60,27 @@ class Server(wx.Frame):
             # 服务器首先接收客户端发送过来的第一条消息，我们规定第一条消息为客户端的名字
             username = session_socket.recv(1024).decode('utf-8')  # 接受客户端名字
             # 创建一个会话线程
-            session_thread = SessionThread(session_socket, username, self)
+            session_thread = SessionThread(self, session_socket, username)
             self.session_thread_map[username] = session_thread
             session_thread.start()
+            # 表示有客户端进入到聊天室
+            self.show_info_and_send_client('服务端通知：', f'欢迎{username}进入到聊天室！',
+                                           time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))
         self.server_socket.close()
+
+    # 在文本中显示聊天信息，同时发送消息给所有的客户端。source:信息源;data:信息
+    def show_info_and_send_client(self, source, data, data_time):
+        send_data = f'{source} : {data}\n时间: {data_time}\n'
+        self.text.AppendText('------------------------------\n%s' % send_data)  # 在服务器的文本框显示信息
+        for client in self.session_thread_map.values():
+            if client.isOn:  # 当前客户端是活动
+                client.user_socket.send(send_data.encode('utf-8'))
+
+    # 服务器保存聊天内容
+    def save_record(self, event):
+        record = self.text.GetValue()
+        with open(file='record.log', mode='w+') as f:
+            f.write(record)
 
 
 # 服务器会话线程的类
@@ -79,8 +98,13 @@ class SessionThread(threading.Thread):
             data = self.user_socket.recv(1024).decode('utf-8')
             if data == 'A^disconnect^B':  # 如果客户端点击断开按钮，先发一条消息给服务器：消息内容的规定：A^disconnect^B
                 self.isOn = False
+                # 有用户离开，需要在聊天室通知其他人
+                self.server.show_info_and_send_client('服务端通知：', f'{self.username}离开聊天室！',
+                                                      time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))
             else:
-                pass  # 其他聊天信息，我们应该显示给所有的客户端，包括服务器
+                # 其他聊天信息，我们应该显示给所有的客户端，包括服务器
+                self.server.show_info_and_send_client(self.username, data,
+                                                      time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))
         self.user_socket.close()
 
 
